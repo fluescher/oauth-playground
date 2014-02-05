e2bfb296befaa5206ed9b29834b806d37e1918e8
Florian Luescher <florian@florianluescher.ch>
2014-02-05 14:16:46 +0100

    Remove static class configuration

--- oauth-server/src/main/java/com/zuehlke/oauth/OAuthApplication.java
+++ oauth-server/src/main/java/com/zuehlke/oauth/OAuthApplication.java
@@ -1,21 +1,8 @@
 package com.zuehlke.oauth;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/")
 public class OAuthApplication extends Application {
-
-    private static final Set<Class<?>> classes = 
-            Collections.unmodifiableSet(new HashSet<Class<?>>(Arrays.asList(SecuredResource.class)));
-    
-    @Override
-    public Set<Class<?>> getClasses() {
-        return classes;
-    }
 }
